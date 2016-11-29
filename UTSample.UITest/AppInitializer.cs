23b5ce929c0c2a7c1ab0b21579f9642cf14aeba6
Damian Kisielewski <namian.Kisielewski@dfc.local>
2016-11-29 14:56:24 +0100

    UI automated test

---
+++ UTSample.UITest/AppInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace UTSample.UITest
+{
+	public class AppInitializer
+	{
+		private const string path = @"C:\App\UT\UTSample\UTSample\UTSample.Droid\bin\Debug\UTSample.Droid.apk";
+		public static IApp StartApp(Platform platform)
+		{
+			return ConfigureApp.Android.StartApp();
+		}
+	}
+}
---
+++ UTSample.UITest/Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+
+namespace UTSample.UITest
+{
+	[TestFixture(Platform.Android, Category ="a")]
+	public class Tests
+	{
+		IApp app;
+		Platform platform;
+		public Tests(Platform platform)
+		{
+			this.platform = platform;
+		}
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = AppInitializer.StartApp(platform);
+			//{
+			//	// TODO: If the Android app being tested is included in the solution then open
+			//	// the Unit Tests window, right click Test Apps, select Add App Project
+			//	// and select the app projects that should be tested.
+			//	app = ConfigureApp
+			//		.Android
+			//		// TODO: Update this path to point to your Android app and uncomment the
+			//		// code if the app is not included in the solution.
+			//		//.ApkFile ("../../../Android/bin/Debug/UITestsAndroid.apk")
+			//		.StartApp();
+		}
+
+		[Test]
+		public void AppLaunches()
+		{
+			//app.Query(c => c.All());
+			//app.Screenshot("First screen.");
+			app.Tap("Cos");
+		}
+	}
+}
+
